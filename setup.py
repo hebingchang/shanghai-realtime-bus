9eb697dfbeac7c0c1d49be9aba9bf64d82750346
hebingchang <hebingchang1@live.com>
2019-01-13 12:45:00 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'shbus',
-    version = '1.0.0',
+    version = '1.0.1',
     keywords = ('pip', 'shanghai', 'realtime', 'bus'),
     description = 'Python implementation of Shanghai Bus App.',
     long_description = '「上海公交」APP 的 Python 实现。基本实现了 APP 中所有功能，如查询线路、实时公交等。',
     license = 'MIT Licence',
 
-    url = 'https://boar.moe',
+    url = 'https://github.com/hebingchang/shanghai-realtime-bus',
     author = 'hebingchang',
     author_email = 'hebingchang1@live.com',
 
